4a383516d26778ae92c7193923688426d3574d7c
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-11 23:19:52 +1100

    df

--- apps/root/data/public/js/user-panel.js
+++ apps/root/data/public/js/user-panel.js
@@ -7,7 +7,9 @@ $(document).ready(
         if (myAccount) {
             html = renderUserPanel(myAccount);
             $('#user-panel').replaceWith(html);
-            
+            $("#user-menu-button").click( e => {
+                $("#user-panel-menu").toggleClass('hidden');
+            });
             let account = JSON.parse(myAccount);
             $(document).trigger('my-account', [account]);
 
@@ -31,7 +33,7 @@ $(document).ready(
         <div>
             <a href="/login?redirect=${currentURL}" type="button"
                 class="flex text-sm rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                id="user-menu-button-2" aria-expanded="false">
+                id="user-menu-button" aria-expanded="false">
                 <span class="sr-only">Open user menu</span>
                 <svg class="w-6 h-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
@@ -56,7 +58,7 @@ $(document).ready(
             <div>
                 <button type="button"
                     class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                    id="user-menu-button-2" aria-expanded="false">
+                    id="user-menu-button" aria-expanded="false">
                     <span class="sr-only">Open user menu</span>
                     <img class="w-8 h-8 rounded-full"
                         src="${picture}" alt="user photo">
@@ -101,11 +103,3 @@ $(document).ready(
 
     }
 );
-
-$(document).on('my-account', (event, account) => {
-    if (account) {
-        $("#user-panel-menu").click( e => {
-            $("#user-panel-menu").toggleClass('hidden');
-        });
-    }
-})
