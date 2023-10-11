18f486c742de4c7f99e906808ed122fbbae7df30
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-11 23:28:57 +1100

    dfd

--- apps/root/data/public/js/user-panel.js
+++ apps/root/data/public/js/user-panel.js
@@ -64,7 +64,7 @@ $(document).ready(
                         src="${picture}" alt="user photo">
                 </button>
             </div>
-            <div class="relative">
+            <div class="relative d">
             <div id="user-panel-menu" class="absolute z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600">
                 <div class="px-4 py-3" role="none">
                     <p class="text-sm text-gray-900 dark:text-white" role="none">
