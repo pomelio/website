033a46a2e9974af7e599723d56109d110509f52d
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-11 22:37:15 +1100

    df

--- apps/root/data/public/js/user-panel.js
+++ apps/root/data/public/js/user-panel.js
@@ -52,7 +52,7 @@ $(document).ready(
             let html = `
         <div id="user-panel" class="flex items-center ml-3">
             <div>
-                <button id="dropdownButton" type="button" data-dropdown-toggle="dropdownMenu"
+                <button type="button"
                     class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                     id="user-menu-button-2" aria-expanded="false">
                     <span class="sr-only">Open user menu</span>
@@ -61,8 +61,7 @@ $(document).ready(
                 </button>
             </div>
 
-            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-                id="dropdownMenu">
+            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600">
                 <div class="px-4 py-3" role="none">
                     <p class="text-sm text-gray-900 dark:text-white" role="none">
                         ${name}
