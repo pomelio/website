0a48e2af37b82736dd6af042d35ad964510d3ff2
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-12 22:47:45 +1100

    df

--- apps/root/data/public/js/comments.js
+++ apps/root/data/public/js/comments.js
@@ -5,8 +5,8 @@ $(document).on('my-account', (event, account) => {
 
     function render(comment_id) {
         let html = `
-    <button
-        class="comment-button-${comment_id} inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    <button id="comment-button-${comment_id}"
+        class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         type="button">
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
             <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
