66e8e917e996ba8738f09610cd437f66a9743cde
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-12 22:44:52 +1100

    sd

--- apps/root/data/public/js/comments.js
+++ apps/root/data/public/js/comments.js
@@ -5,8 +5,8 @@ $(document).on('my-account', (event, account) => {
 
     function render(comment_id) {
         let html = `
-    <button data-dropdown-toggle="dropdownComment-${comment_id}"
-        class="dropdownCommentButton inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    <button
+        class="comment-button-${comment_id} inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         type="button">
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
             <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
@@ -14,7 +14,7 @@ $(document).on('my-account', (event, account) => {
         <span class="sr-only">Comment settings</span>
     </button>
     <!-- Dropdown menu -->
-    <div id="dropdownComment-${comment_id}"
+    <div id="dropdown-comment-${comment_id}"
         class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
         <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
             aria-labelledby="dropdownMenuIconHorizontalButton">
@@ -88,6 +88,9 @@ $(document).on('my-account', (event, account) => {
             if (user_id == account.id) {
                 let html = render(comment_id);
                 $(this).replaceWith(html);
+                $('#comment-button-' + comment_id).click(e => {
+                    $('#dropdown-comment-' + comment_id).toggleClass('hidden');
+                });
 
                 $("#edit-" + comment_id).click(e => {
                     //e.preventDefault();
