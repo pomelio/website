cf373e1766cfe0cb8e2ace03d0290851828a246e
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-12 13:17:51 +1100

    ddf

--- apps/blog/data/public/js/follower-pannel.js
+++ apps/blog/data/public/js/follower-pannel.js
@@ -6,7 +6,7 @@ $(document).on('my-account', (event, account) => {
     <button id="follower-toggle" type="button" class="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Followers
         <span class="sr-only">Followers</span>
-        <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${followers.length}</div>
+        <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${followers_count}</div>
     </button>
 `;
 
