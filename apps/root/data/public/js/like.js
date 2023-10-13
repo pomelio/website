0dd1ea96536c9013369bb7ca5716de6aaec01208
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-13 22:07:22 +1100

    sd

--- apps/root/data/public/js/like.js
+++ apps/root/data/public/js/like.js
@@ -22,7 +22,7 @@ $(document).on('my-account', (event, account) => {
     <button id="like-toggle" type="button" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
         ${like_svg}
         <span class="sr-only">Likes</span>
-        <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${followers_count}</div>
+        <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${likes_count}</div>
     </button>
 `;
 
@@ -33,7 +33,7 @@ $(document).on('my-account', (event, account) => {
         <a href="${loginURL}" type="button" class="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Followers
             <span class="sr-only">Followers</span>
-            <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${followers_count}</div>
+            <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${likes_count}</div>
         </a>
 `;
         }
